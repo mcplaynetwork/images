6b965c7ed1ccd88f4c122b9f30acf55b1531cfe1
snake <admin@viasnake.com>
2022-10-09 21:51:00 +0900

    Add java

---
+++ java/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TZ=${TZ:-JST}
+export TZ
+
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+cd /home/container || exit 1
+
+printf "\033[1m\033[33mcontainer@mcplaynetwork~ \033[0mjava -version\n"
+java -version
+
+if [ -d .git ]; then
+printf "\033[1m\033[33mcontainer@mcplaynetwork~ \033[0mG.git directory exists\n"
+printf "\033[1m\033[33mcontainer@mcplaynetwork~ \033[0mGUpdate git upstream\n"
+git add .
+git commit -m "Upstream update" -m "`git diff --name-only --staged`" -m "Generated on `date +'%Y-%m-%d %H:%M:%S'`"
+git push
+fi
+
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+printf "\033[1m\033[33mcontainer@mcplaynetwork~ \033[0m%s\n" "$PARSED"
+exec env ${PARSED}
